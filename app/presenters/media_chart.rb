e453d393971f3f2aef62e705d94ee1d2c9771209
Stephen Potenza <potenza@gmail.com>
2014-01-10 16:26:22 -0800

    slight tweak to media chart

--- app/presenters/media_chart.rb
+++ app/presenters/media_chart.rb
@@ -1,7 +1,7 @@
 class MediaChart
   include Chart
 
-  attr_reader :media
+  attr_reader :media, :date_range
 
   def query(media, start_date, end_date)
     media = [media] if media.kind_of? String
@@ -14,6 +14,7 @@ class MediaChart
 
   def generate_stats
     Time.use_zone(time_zone) do
+      @date_range = start_date.beginning_of_day..end_date.end_of_day
       media.collect do |medium|
         stats = medium_stats(medium)
         stats[:conversion_rate] = stats[:visits] > 0 && (stats[:conversions].to_f / stats[:visits].to_f * 100).round(2) || 0
@@ -24,7 +25,6 @@ class MediaChart
   end
 
   def medium_stats(medium)
-    date_range = start_date.beginning_of_day..end_date.end_of_day
     relation = site.tracking_links.where(medium: medium)
     {
       medium: medium,
