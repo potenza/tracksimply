e90407a42205c4a99529a0360cfe21969eea1407
Stephen Potenza <potenza@gmail.com>
2014-01-10 16:30:10 -0800

    one more minor refactoring for readability

--- app/presenters/media_chart.rb
+++ app/presenters/media_chart.rb
@@ -6,21 +6,25 @@ class MediaChart
   def query(media, start_date, end_date)
     media = [media] if media.kind_of? String
     @media = media
-    parse_dates(start_date, end_date)
+    set_date_range(start_date, end_date)
     generate_stats
   end
 
   private
 
-  def generate_stats
+  def set_date_range(start_date, end_date)
+    parse_dates(start_date, end_date)
     Time.use_zone(time_zone) do
-      @date_range = start_date.beginning_of_day..end_date.end_of_day
-      media.collect do |medium|
-        stats = medium_stats(medium)
-        stats[:conversion_rate] = stats[:visits] > 0 && (stats[:conversions].to_f / stats[:visits].to_f * 100).round(2) || 0
-        stats[:profit] = stats[:revenue] - stats[:cost]
-        stats
-      end
+      @date_range = @start_date.beginning_of_day..@end_date.end_of_day
+    end
+  end
+
+  def generate_stats
+    media.collect do |medium|
+      stats = medium_stats(medium)
+      stats[:conversion_rate] = stats[:visits] > 0 && (stats[:conversions].to_f / stats[:visits].to_f * 100).round(2) || 0
+      stats[:profit] = stats[:revenue] - stats[:cost]
+      stats
     end
   end
 
