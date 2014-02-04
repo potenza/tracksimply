ed13d65676d3fe71b3c77dae2c4ded5f9b0fecec
Stephen Potenza <potenza@gmail.com>
2014-02-03 16:14:13 -0800

    slight refactor for sites controller

--- app/controllers/api/sites_controller.rb
+++ app/controllers/api/sites_controller.rb
@@ -8,9 +8,14 @@ class Api::SitesController < ApplicationController
 
   def table
     site = Site.find(params[:id])
-    aggregate_by = params[:aggregate_by]
-    aggregate_by = "medium" if aggregate_by == "media"
     stats = SitePerformanceAggregator.new(site, current_user.time_zone, params[:start_date], params[:end_date], aggregate_by, params[:filters]).query
     render json: SitePerformancePresenter.new(stats).data.to_json
+  end
+
+  private
+
+  def aggregate_by
+    aggregate_by = params[:aggregate_by]
+    aggregate_by = "medium" if aggregate_by == "media"
   end
 end
